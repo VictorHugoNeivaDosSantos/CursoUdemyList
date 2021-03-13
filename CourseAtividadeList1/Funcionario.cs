9fce973aed7b60abec4595843249156ae28f3f65
VictorHugoNeivadosSantos <victorhugoneiva2017@gmail.com>
2021-03-13 12:51:56 -0300

    Lista

---
+++ CourseAtividadeList1/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseAtividadeList1
+{
+    class Funcionario { 
+
+
+        public string Nome { get; set; }
+    public int IDFuncionario { get; set; }
+    public double SalarioFuncionario { get; set; }
+
+
+    public Funcionario(int idFun, string nomeFun, double salarioFun)
+    {
+
+        Nome = nomeFun;
+        IDFuncionario = idFun;
+        SalarioFuncionario = salarioFun;
+
+    }
+
+
+    public void AumentarSalarioFuncionario(double aumentoPorcentagem)
+    {
+
+        SalarioFuncionario = SalarioFuncionario * ((100 + aumentoPorcentagem) / 100);
+
+    }
+
+
+    public override string ToString()
+    {
+        return "#" + IDFuncionario + ", " + Nome + ", tem o salário de R$" + SalarioFuncionario;
+    }
+
+
+
+
+
+}
+    }
+}
---
+++ CourseAtividadeList1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CourseAtividadeList1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> lista = new List<Funcionario>();
+
+            Boolean inicio = true;
+            while (inicio == true)
+            {
+                Console.WriteLine();
+                Console.Write("1. Cadastro, 2.Aumentar Salário, 3.Sair: ");
+                int opc = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (opc == 1)
+                {
+                    Console.Write("Digite o nome:");
+                    string nome = Console.ReadLine();
+                    Console.Write("Digite o ID: ");
+                    int ID = int.Parse(Console.ReadLine());
+                    Console.Write("Digite o salário:");
+                    double salario = double.Parse(Console.ReadLine());
+                    lista.Add(new Funcionario(ID, nome, salario));
+                    Console.WriteLine("--------------------Funcionário cadastrado!!!----------------------");
+                    Console.WriteLine();
+
+                }
+                else if (opc == 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("-----------------Localizando o Funcionário para aumento------------------");
+                    Console.Write("Digite o ID do funcionario: ");
+                    int ID = int.Parse(Console.ReadLine());
+                    Funcionario fun = lista.Find(x => x.IDFuncionario == ID);
+                    Console.WriteLine(fun);
+
+                    if (fun != null)
+                    {
+                        Console.WriteLine("------------------Funcionário encontrado!----------------");
+                        Console.WriteLine();
+                        Console.Write("Digite a porcentagem de aumento: ");
+                        double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        fun.AumentarSalarioFuncionario(porcent);
+                        Console.WriteLine(fun);
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Funcionário não encontrado!");
+                        Console.WriteLine();
+
+                    }
+                }
+                else if (opc == 3)
+                {
+                    Console.WriteLine();
+
+                    Console.WriteLine("Fim do programa!!!");
+                    inicio = false;
+                    Console.WriteLine();
+                }
+
+
+                foreach (var item in lista)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+
+
+
+            }
+
+        }
+
+    }
+    }
+}
